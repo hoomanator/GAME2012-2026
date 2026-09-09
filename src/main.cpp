bb5358d4bb60817229f2442abf4bf9da8f7bb89f
101034096 <101034096@WFD202IT39>
2026-09-09 17:14:26 -0400

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+
+int main(void)
+{
+
+    constexpr int SCREEN_WIDTH = 800;
+    constexpr int SCREEN_HEIGHT = 600;
+
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello OpenGL", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Load OpenGL 4.6*/
+    gladLoadGL();
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glClearColor(0.49f, 0.29f, 0.19f, 1.0f);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
