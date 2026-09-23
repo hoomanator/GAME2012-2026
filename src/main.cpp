1739854bf527ebc6e6e2345654ac40815891b49e
Hooman Salamat <hooman.salamat@georgebrown.ca>
2026-09-23 17:03:53 -0400

    VBO and VAO of my vertices are completed

--- src/main.cpp
+++ src/main.cpp
@@ -34,5 +34,55 @@ int main()
 {
     CreateWindow(800, 800, "Graphics 1");
 
+    GLuint a1_tri_vert = CreateShader(GL_VERTEX_SHADER, "./assets/shaders/a1_triangle.vert");
+    GLuint a1_tri_frag = CreateShader(GL_FRAGMENT_SHADER, "./assets/shaders/a1_triangle.frag");
+    GLuint a1_tri_shader = CreateProgram(a1_tri_vert, a1_tri_frag);
+
+    GLuint vertex_buffer_rainbow_positions;
+    GLuint vertex_buffer_rainbow_colors;
+    glGenBuffers(1, &vertex_buffer_rainbow_positions);
+    glGenBuffers(1, &vertex_buffer_rainbow_colors);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_rainbow_positions);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_positions), vertex_positions, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_rainbow_colors);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_colors), vertex_colors, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, GL_NONE);
+    // Can only have 1 vertex buffer bound at a time, so must unbind in order to prevent overwriting it
+
+    GLuint vertex_buffer_white;
+    glGenBuffers(1, &vertex_buffer_white);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_white);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_white), vertices_white, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, GL_NONE);
+
+    GLuint vertex_array_rainbow;
+    glGenVertexArrays(1, &vertex_array_rainbow);
+    glBindVertexArray(vertex_array_rainbow);
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_rainbow_positions);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vector2), 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_rainbow_colors);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vector3), 0);
+
+    glBindVertexArray(GL_NONE);
+
+    GLuint vertex_array_white;
+    glGenVertexArrays(1, &vertex_array_white);
+    glBindVertexArray(vertex_array_white);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_white);
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, pos));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, col));
+
+    glBindVertexArray(GL_NONE);
+
+
     return 0;
 }
